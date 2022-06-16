b66924c9c50edb6fd1740390059e9cc0692bf760
WatanabeToshimitsu <toshimitsu.watanabe@hdwlab.co.jp>
2022-06-16 18:49:57 +0900

    Fix CI

--- dev-tools-for-typescript/configs/jest.config.js
+++ dev-tools-for-typescript/configs/jest.config.js
@@ -4,12 +4,12 @@ module.exports = {
   setupFilesAfterEnv: ["./src/setupTest.ts"],
   testEnvironment: "jsdom",
   verbose: true,
-  // transform: {
-  //   "^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
-  //     "jest-transform-stub",
-  // },
-  // moduleNameMapper: {
-  //   "^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
-  //     "jest-transform-stub",
-  // },
+  transform: {
+    "^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
+      "jest-transform-stub",
+  },
+  moduleNameMapper: {
+    "^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
+      "jest-transform-stub",
+  },
 };
